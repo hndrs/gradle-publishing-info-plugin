24a5b2f318ee33c35a8ecb9c89ae901234c8ae69
Marvin Schramm <marvin@kreait.com>
2021-02-15 10:34:35 +0100

    rename plugin identifier

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,25 +18,24 @@ repositories {
 gradlePlugin {
     plugins {
         publishingInfoPlugin = register("publishingInfoPlugin") {
-            id = "io.hndrs.gradle.publishing.info"
+            id = "io.hndrs.publishing-info"
             implementationClass = "io.hndrs.gradle.plugin.PublishingInfoPlugin"
         }
     }
 }
 
-val tagList = listOf("maven", "publish", "repository", "gradle", "library", "pom", "pom.xml")
+val tagList = listOf("maven", "publish", "repository", "gradle", "library", "pom", "pom.xml", "plugins")
 
 pluginBundle {
     website = "https://github.com/hndrs/gradle-publishing-info-plugin"
     vcsUrl = "https://github.com/hndrs/gradle-publishing-info-plugin.git"
     description = "Simplifies adding publishing meta data to maven publications"
     tags = tagList
-
     (plugins) {
         // first plugin
         "publishingInfoPlugin" {
             // id is captured from java-gradle-plugin configuration
-            displayName = "Gradle publishing info plugin"
+            displayName = "Gradle Publishing Info plugin"
             tags = tagList
             version = rootProject.version as? String
         }
