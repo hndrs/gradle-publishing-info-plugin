c531411071a448d0723747b95a1d4fc2ca0d19e5
Marvin Schramm <marvin@kreait.com>
2021-10-23 16:41:34 +0200

    remove uncessary plugin configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
 pluginManagement {
     val kotlinVersion = "1.5.30"
     plugins {
-        id("io.spring.dependency-management").version("2.4.2")
         kotlin("jvm").version(kotlinVersion)
-        kotlin("plugin.spring").version(kotlinVersion)
         kotlin("kapt").version(kotlinVersion)
         id("maven-publish")
         id("idea")
